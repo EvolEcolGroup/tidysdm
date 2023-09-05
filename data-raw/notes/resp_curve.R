4311d3feebc81aeb3fcd21786fce26f2621fb80c
Andrea Manica <am315@cam.ac.uk>
2023-09-05 17:02:50 +0100

    early dalex function implementation

--- data-raw/notes/resp_curve.R
+++ data-raw/notes/resp_curve.R
@@ -68,15 +68,91 @@ plot(vip_wkflow)
 
 ## could create a plot function for a list of model parts with the function at
 ## https://www.tmwr.org/explain
-
-custom_predict <- function(X.model, newdata) {
-  predict(X.model, newdata)$mean
+explain_tidysdm <- function(
+    model,
+    data = NULL,
+    y = NULL,
+    predict_function = NULL,
+    predict_function_target_column = NULL,
+    residual_function = NULL,
+    label = NULL,
+    verbose = TRUE,
+    precalculate = TRUE,
+    colorize = !isTRUE(getOption("knitr.in.progress")),
+    model_info = NULL,
+    type = "classification",
+    by_workflow = FALSE
+) {
+  if (!inherits(model,"simple_ensemble")){
+    stop("this function currently only works with simple_ensembles from tidysdm")
+  }
+  if (is.null(data)){
+    data = extract_mold(lacerta_ensemble$workflow[[1]])$predictors
+  }
+  if (is.null(y)){
+    # note that we need presences to be 1 and absences to be zero
+    y <- (as.numeric(extract_mold(lacerta_ensemble$workflow[[1]])$outcomes %>% pull())-2)*-1
+  } else {
+    if (!is.factor(y)){
+      stop("y should be a factor with presences as reference levels")
+    } else {
+      y <- (as.numeric(y)-2)*-1
+    }
+  }
+  if (type!="classification"){
+    stop("type has to be classification for a tidysdm ensemble")
+  }
+  if (is.null(predict_function)){
+    predict_function <- function(model, newdata) {
+      predict(model, newdata)$mean
+    }
+  }
+  
+  model_info <- list(package = "tidysdm",
+                     ver = utils::packageVersion("tidysdm"),
+                     type = "classification")
+  
+  DALEX::explain(
+    model = model,
+    data = data,
+    y = y,
+    predict_function = predict_function,
+    predict_function_target_column = predict_function_target_column,
+    residual_function = residual_function,
+    weights = NULL,
+    label = label,
+    verbose = verbose,
+    precalculate = precalculate,
+    colorize = colorize,
+    model_info = NULL,
+    type = type
+  )
 }
 
-explainer_ensemble <- explain(lacerta_ensemble, 
-                              data = data_train,
-                              y = (data_response-1)*-1,
-                              predict_function = custom_predict,
-                              type = "classification")
-vip_ensemble <- model_parts(explainer = explainer_ensemble)
+# method for model info to work on simple ensemble
+#' @rdname model_info
+#' @export
+model_info.simple_ensemble<- function(model, is_multiclass=FALSE,...){
+  if (is_multiclass){
+    stop("tidysdm simple_ensembles can not be multiclass")
+  }
+  package <- "tidysdm"
+  type <- "classification"
+  ver <- try(as.character(utils::packageVersion(package)), 
+             silent = TRUE)
+  if (inherits(ver, "try-error")) {
+    ver <- "Unknown"
+  }
+  model_info <- list(package = package, ver = ver, type = type)
+  class(model_info) <- "model_info"
+  model_info
+}
+
+# code examples
+explainer_lacerta_ens <- explain_tidysdm(lacerta_ensemble)
+vip_ensemble <- model_parts(explainer = explainer_lacerta_ens)
 plot(vip_ensemble)
+
+pdp_bio05 <- model_profile(explainer_lacerta_ens, N = 500, variables = "bio05")
+plot(pdp_bio05)
+
