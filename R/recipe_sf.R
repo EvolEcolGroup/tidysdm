8ac0b00ff2e3522b415280d855284c7d65637734
Andrea Manica <am315@cam.ac.uk>
2023-09-26 08:48:36 +0100

    tests and documentation for spatial_recipe

--- R/recipe_sf.R
+++ R/recipe_sf.R
@@ -2,7 +2,7 @@
 #'
 #' This method for [recipes::recipe()] handles the
 #' case when `x` is an [sf::sf] object, as commonly
-#' used in Species Distribution Model.
+#' used in Species Distribution Model, and generates a `spatial_recipe`.
 #'
 #' [recipes] are not natively compatible with [sf::sf] objects. The problem is that
 #' the `geometry` column of [sf::sf] objects is a list, which is incompatible with
@@ -10,7 +10,10 @@
 #' column from the [data.frame] and replaces it with a simple `X` and `Y` columns
 #' before any further operations, thus allowing
 #' the usual processing by [recipe()] to succeed (`X` and `Y` are give the role
-#' of coords ina spatial recipe).
+#' of coords ina spatial recipe). When prepping and baking a `spatial_recipe`,
+#' if a data.frame or tibble without coordinates is used as `training` or 
+#' `new_data`, dummy `X` and `Y` columns
+#' are generated and filled with NAs.
 #' NOTE that order matters! You need to use the syntax
 #' `recipe(x=sf_obj, formula=class~.)` for the method to successfully detect
 #' the [sf::sf] object. Starting with `formula` will fail.
@@ -34,4 +37,13 @@ recipe.sf <- function (x, ...) {
     update_role(dplyr::any_of(c("X","Y")),new_role="coords")
   class(rec) <- c("spatial_recipe", class(rec))
   rec
+}
+
+#' @export
+#' @rdname recipe.sf
+spatial_recipe <- function (x, ...){
+  if (!inherits(x, "sf")){
+    stop("x should be an `sf` object")
+  }
+  recipe(x, ...)
 }
