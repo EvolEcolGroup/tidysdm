bb1a9bcac602973d764dc2eddb547d2bb8030d9d
Andrea Manica <am315@cam.ac.uk>
2023-11-10 10:17:10 +0000

    Further lower thread count

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,10 @@
 .onLoad <- function(libname, pkgname) {
   # CRAN OMP THREAD LIMIT
-  # if OMP limit is unset, set it to two
+  # if OMP limit is unset, set it to 1
   # if the user has already set it to something else, leave it alone
   if (Sys.getenv("OMP_THREAD_LIMIT")=="") {
-    Sys.setenv("OMP_THREAD_LIMIT" = 2)
+    Sys.setenv("OMP_THREAD_LIMIT" = 1)
   }
-  
   # This defines maxent in the model database
   current <- parsnip::get_model_env()
   # only set the model if it does not exist yet
