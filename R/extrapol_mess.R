e0471b6dd885926f433aff45a4f55a31d641e767
Andrea Manica <am315@cam.ac.uk>
2025-01-31 09:53:11 +0000

    Update coverage tracking

--- R/extrapol_mess.R
+++ R/extrapol_mess.R
@@ -1,17 +1,17 @@
 #' Multivariate environmental similarity surfaces (MESS)
-#' 
+#'
 #' Compute multivariate environmental similarity surfaces (MESS), as described
 #' by Elith et al., 2010.
-#' 
-#' This function is a modified version of `mess` in 
+#'
+#' This function is a modified version of `mess` in
 #' package `predicts`, with a method added to work on [`terra::SpatRasterDataset`].
 #' Note that the method for [`terra::SpatRasterDataset`] assumes that each variables
 #' is stored as a [`terra::SpatRaster`] with time information within `x`. Time
 #' is also assumed to be in `years`. If these conditions are not met, it is possible
-#' to manually extract a [`terra::SpatRaster`] for each time step, and use 
+#' to manually extract a [`terra::SpatRaster`] for each time step, and use
 #' `extrapol_mess` on those [`terra::SpatRaster`]s
-#' 
-#' 
+#'
+#'
 #' @param x [`terra::SpatRaster`], `stars`, [`terra::SpatRasterDataset`] or [`data.frame`]
 #' @param training matrix or data.frame or sf object containing the reference values; each column
 #' should correspond to one layer of the [`terra::SpatRaster`] object, with the exception
@@ -68,12 +68,12 @@ extrapol_mess.SpatRaster <- function(x, training, .col, filename="", ...) {
 	  if (!setequal(names(training),names(x))){
 	    stop("`x` and `training` should contain the same variables")
 	  }
-	  
+
 		training <- stats::na.omit(training)
 		if (nrow(training) < 2) {
 			stop("insufficient number of reference points")
 		}
-    
+
 		out <- terra::rast(x)
 		nl <- terra::nlyr(x)
 		nms <- paste0(names(x), "_mess")
@@ -82,7 +82,7 @@ extrapol_mess.SpatRaster <- function(x, training, .col, filename="", ...) {
 		if (nl == 1) {
 			names(out) <- "mess"
 			b <- terra::writeStart(out, filename, ...)
-			for (i in 1:b$n) {		
+			for (i in 1:b$n) {
 				vv <- terra::readValues(x, b$row[i], b$nrows[i])
 				p <- .messi(vv, training)
 				terra::writeValues(out, p, b$row[i], b$nrows[i])
@@ -101,7 +101,7 @@ extrapol_mess.SpatRaster <- function(x, training, .col, filename="", ...) {
 		}
 		terra::writeStop(out)
 		out
-	}	
+	}
 
 
 #' @rdname extrapol_mess
@@ -124,19 +124,19 @@ extrapol_mess.data.frame <- function(x, training, .col, ...) {
 	  if (!setequal(names(training),names(x))){
 	    stop("`x` and `training` should contain the same variables")
 	  }
-	  
+
 	  training <- stats::na.omit(training)
 	  if (nrow(training) < 2) {
 	    stop("insufficient number of reference points")
 	  }
-	  
+
 		if (ncol(x) == 1) {
 			data.frame(mess=.messi(x, training))
 		} else {
 			x <- sapply(1:ncol(x), function(i) .messi(x[,i], training[,i]))
 			rmess <- apply(x, 1, min, na.rm=TRUE)
 				data.frame(mess=rmess)
-		}	
+		}
 	}
 
 
@@ -153,10 +153,10 @@ extrapol_mess.SpatRasterDataset <- function(x, training, .col, ...) {
 	  if (.col!=""){
 	    training <- training %>% dplyr::select(-dplyr::one_of(.col))
 	  }
-	  
+
   # check this is a valid region_series
   # TODO
-	  
+
 	# get times from the first layer
 	if (!terra::timeInfo(x[[1]])$time){
 	  stop("The rasters in `SpatRasterDataset`x`` have no time info; define times with terra::time")
@@ -165,7 +165,7 @@ extrapol_mess.SpatRasterDataset <- function(x, training, .col, ...) {
 	    stop("The time step of `x` is not `years`; you will need to extract one time step at a time manually and compute the MESS on each SpatRaster")
 	  }
 	raster_times <- terra::time(x[[1]])
-	
+
 	# null mess raster to fill in
 	mess_rast <- NULL
   # cycle over the time steps
@@ -196,23 +196,14 @@ extrapol_mess.SpatRasterDataset <- function(x, training, .col, ...) {
 	f <- 100 * findInterval(p, v) / length(v)
 	minv <- v[1]
 	maxv <- v[length(v)]
-	res <- 2*f 
+	res <- 2*f
 	f[is.na(f)] <- -99
 	i <- f>50 & f<100
 	res[i] <- 200-res[i]
 
-	i <- f==0 
+	i <- f==0
 	res[i] <- 100*(p[i]-minv)/(maxv-minv)
 	i <- f==100
 	res[i] <- 100*(maxv-p[i])/(maxv-minv)
 	res
 }
-
-.messix <- function(p,v) {
-# a little bit different, no negative values.
-	a <- stats::ecdf(v)(p)
-	a[a>0.5] <- 1-a[a>0.5]
-	200 * a
-}
-
-
