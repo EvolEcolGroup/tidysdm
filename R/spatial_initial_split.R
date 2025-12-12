a45284a00e9a4baa558ba407ea7c6a88a7e4e548
Andrea Manica <am315@cam.ac.uk>
2025-12-12 16:42:56 +0000

    fix linting

--- R/spatial_initial_split.R
+++ R/spatial_initial_split.R
@@ -5,11 +5,11 @@
 #' are available; a random split from that strategy will be used to generate the
 #' initial split.
 #' @param data A dataset (data.frame or tibble)
-#' @param prop The proportion of data to be retained for modelling/analysis. This
-#' parameter is used to define the appropriate number of partitions for the
-#' selected `strategy`. For example, if `prop = 0.2`, then 5 partitions will be
-#' created and one of these will be used as the testing set. Set to NULL for
-#' leave-one-out crossvalidation.
+#' @param prop The proportion of data to be retained for modelling/analysis.
+#'   This parameter is used to define the appropriate number of partitions for
+#'   the selected `strategy`. For example, if `prop = 0.2`, then 5 partitions
+#'   will be created and one of these will be used as the testing set. Set to
+#'   NULL for leave-one-out crossvalidation.
 #' @param strategy A sampling strategy from `spatialsample`
 #' @param ... parameters to be passed to the `strategy`
 #' @returns An `rsplit` object that can be used with the [rsample::training] and
