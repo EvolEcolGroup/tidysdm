4c7d72ae8afc64269fe08381409ac2c136888469
Andrea Manica <am315@cam.ac.uk>
2025-12-29 17:35:27 +0000

    One final test for codecov

--- tests/testthat/test_explain_tidysdm.R
+++ tests/testthat/test_explain_tidysdm.R
@@ -31,6 +31,11 @@ test_that("we can explain tidysdm objects", {
     "type has to be classification for a tidysdm ensemble"
   )
   expect_error(
+    explain_tidysdm(tidysdm::lacerta_ensemble, type = "regression",
+                    by_workflow = TRUE),
+    "type has to be classification for a tidysdm ensemble"
+  )
+  expect_error(
     explain_tidysdm(lacerta_rep, type = "regression"),
     "type has to be classification for a tidysdm ensemble"
   )
