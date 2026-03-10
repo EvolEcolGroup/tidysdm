8f2eedef7bfabeb611cef80a41db41f3ef295905
OndraPelanek <ondrap2@gmail.com>
2026-03-10 16:55:36 +0000

    AI suggestion for a new predict.repeat_ensemble
    So far untested and spagetti

--- R/predict_repeat_ensemble.R
+++ R/predict_repeat_ensemble.R
@@ -58,47 +58,304 @@
 #   }
 
 
+# predict.repeat_ensemble <- function(object,
+#                                     new_data,
+#                                     type = "prob",
+#                                     fun = "mean",
+#                                     metric_thresh = NULL,
+#                                     class_thresh = NULL,
+#                                     members = FALSE,
+#                                     ...) {
+#   # store unique repeat ids
+#   rep_ids <- unique(object$rep_id)
+# 
+#   # predict within each repeat (aggregate members within repeat)
+#   pred_per_rep <- lapply(rep_ids, function(rep_id) {
+#     obj_rep <- object[object$rep_id == rep_id, , drop = FALSE]
+# 
+#     # reuse simple_ensemble predict method
+#     class(obj_rep)[1] <- "simple_ensemble"
+# 
+#     stats::predict(
+#       object = obj_rep,
+#       new_data = new_data,
+#       type = "prob",
+#       fun = fun,
+#       metric_thresh = metric_thresh,
+#       class_thresh = class_thresh,
+#       ...
+#     )
+#   })
+# 
+#   # set names of the list to the unique repeat ids
+#   names(pred_per_rep) <- rep_ids
+# 
+#   # average repeat level predictions across repeats (equal weight per repeat)
+#   out <- lapply(fun, function(col) {
+#     mat <- do.call(cbind, lapply(pred_per_rep, function(x) { x[[col]] }))
+#     rowMeans(mat, na.rm = TRUE)
+#   })
+#   names(out) <- fun
+# 
+#   return(as.data.frame(out))
+# }
+
+
 predict.repeat_ensemble <- function(object,
                                     new_data,
                                     type = "prob",
                                     fun = "mean",
                                     metric_thresh = NULL,
+                                    metric_thresh_repeat = NULL,
                                     class_thresh = NULL,
                                     members = FALSE,
                                     ...) {
+  # type check
+  if (!type %in% c("prob", "class")) {
+    stop("'type' can only take values 'prob' or 'class'")
+  }
+  
+  if (type == "class") {
+    if (fun[1] == "none") {
+      stop(
+        "classes can be generated only if an aggregating ",
+        "function is given"
+      )
+    }
+    if (members) {
+      message("classes are only provided for aggregated ensemble predictions")
+      members <- FALSE
+    }
+    if (is.null(class_thresh)) {
+      message("as no 'threshold' was defined, a default of 0.5 will be used")
+      class_thresh <- 0.5
+    }
+    
+    if (!is.numeric(class_thresh)) {
+      ref_calib_tb <- attr(object, "class_thresholds")
+      if (is.null(ref_calib_tb)) {
+        stop(
+          "this model needs to be first calibrated before classes can ",
+          "be produced\n",
+          "use 'calib_class_thresh()' first"
+        )
+      } else if (!any(unlist(lapply(
+        ref_calib_tb %>% dplyr::pull("metric_thresh"),
+        identical,
+        metric_thresh_repeat
+      )) &
+      unlist(lapply(
+        ref_calib_tb %>% dplyr::pull("class_thresh"),
+        identical,
+        class_thresh
+      )))) {
+        stop(
+          "this model needs to be first calibrated before classes can ",
+          "be produced\n",
+          "use 'calib_class_thresh()' first"
+        )
+      }
+      
+      ref_calib_tb <- ref_calib_tb[
+        (unlist(lapply(
+          ref_calib_tb %>% dplyr::pull("metric_thresh"),
+          identical,
+          metric_thresh_repeat
+        )) &
+          unlist(lapply(
+            ref_calib_tb %>% dplyr::pull("class_thresh"),
+            identical,
+            class_thresh
+          ))) &
+          ref_calib_tb$fun %in% fun,
+      ]
+    } else {
+      ref_calib_tb <- tibble::tibble(
+        fun = fun,
+        optim_value = rep(class_thresh, length(fun))
+      )
+    }
+    
+    class_levels <- levels(
+      workflows::extract_mold((object$workflow[[1]]))$outcome %>%
+        dplyr::pull(1)
+    )
+  }
+  
+  # set up the aggregating function
+  have_fun <- TRUE
+  if (inherits(fun, "character")) {
+    if (!(((length(fun) == 1 && fun[1] == "none")) ||
+          all(fun %in% c(
+            "mean", "median", "weighted_mean",
+            "weighted_median"
+          )))) {
+      stop(
+        "fun should be either 'none', or a combination of 'mean', ",
+        "'median', 'weighted_mean', and 'weighted_median'"
+      )
+    }
+    if (fun[1] == "none") {
+      have_fun <- FALSE
+      members <- TRUE
+    }
+  } else {
+    stop(
+      "fun should be either 'none', or a combination of 'mean', ",
+      "'median', 'weighted_mean', and 'weighted_median'"
+    )
+  }
+  
   # store unique repeat ids
   rep_ids <- unique(object$rep_id)
-
-  # predict within each repeat (aggregate members within repeat)
-  pred_per_rep <- lapply(rep_ids, function(rep_id) {
+  
+  # helper to get repeat-level metric values
+  get_repeat_metric <- function(obj_rep, metric_name, metric_thresh_member = NULL) {
+    metric_rep <-
+      dplyr::bind_rows(obj_rep$metrics) %>%
+      dplyr::filter(.data$.metric == metric_name)
+    
+    if (!is.null(metric_thresh_member)) {
+      metric_rep <- metric_rep[metric_rep$mean > as.numeric(metric_thresh_member[2]), ]
+    }
+    
+    if (nrow(metric_rep) == 0) {
+      return(NA_real_)
+    }
+    
+    mean(metric_rep$mean)
+  }
+  
+  # predict within each repeat
+  pred_per_rep <- list()
+  metric_repeat <- numeric(length(rep_ids))
+  
+  for (i_rep in seq_along(rep_ids)) {
+    rep_id <- rep_ids[i_rep]
     obj_rep <- object[object$rep_id == rep_id, , drop = FALSE]
-
+    
     # reuse simple_ensemble predict method
     class(obj_rep)[1] <- "simple_ensemble"
-
-    stats::predict(
+    
+    pred_per_rep[[i_rep]] <- stats::predict(
       object = obj_rep,
       new_data = new_data,
       type = "prob",
       fun = fun,
       metric_thresh = metric_thresh,
       class_thresh = class_thresh,
+      members = members,
       ...
     )
-  })
-
-  # set names of the list to the unique repeat ids
+    
+    # repeat-level metric for filtering / weighting across repeats
+    if (!is.null(metric_thresh_repeat)) {
+      metric_repeat[i_rep] <-
+        get_repeat_metric(
+          obj_rep = obj_rep,
+          metric_name = metric_thresh_repeat[1],
+          metric_thresh_member = metric_thresh
+        )
+    } else {
+      metric_repeat[i_rep] <-
+        get_repeat_metric(
+          obj_rep = obj_rep,
+          metric_name = attr(object, "best_metric"),
+          metric_thresh_member = metric_thresh
+        )
+    }
+  }
+  
   names(pred_per_rep) <- rep_ids
-
-  # average repeat level predictions across repeats (equal weight per repeat)
-  out <- lapply(fun, function(col) {
-    mat <- do.call(cbind, lapply(pred_per_rep, function(x) { x[[col]] }))
-    rowMeans(mat, na.rm = TRUE)
-  })
-  names(out) <- fun
-
-  return(as.data.frame(out))
+  
+  # filter repeats if requested
+  if (!is.null(metric_thresh_repeat)) {
+    if (!metric_thresh_repeat[1] %in% attr(object, "metrics")) {
+      stop(
+        "'metric_thresh_repeat' is not among the metrics estimated ",
+        "for this ensemble"
+      )
+    }
+    
+    keep_rep <- metric_repeat > as.numeric(metric_thresh_repeat[2])
+    pred_per_rep <- pred_per_rep[keep_rep]
+    metric_repeat <- metric_repeat[keep_rep]
+    
+    if (length(pred_per_rep) == 0) {
+      stop("the current metric_thresh_repeat excludes all repeats")
+    }
+  }
+  
+  # weighted functions across repeats
+  weighted_mean <- function(x, w = metric_repeat) { # nolint
+    stats::weighted.mean(x, weights = w)
+  }
+  
+  weighted_median <- function(x, w = metric_repeat) { # nolint
+    w <- w[order(x)]
+    x <- x[order(x)]
+    prob <- cumsum(w) / sum(w)
+    ps <- which(abs(prob - .5) == min(abs(prob - .5)))
+    return(x[ps])
+  }
+  
+  # aggregate across repeats
+  if (have_fun) {
+    pred_ensemble <- list()
+    
+    for (i_fun in fun) {
+      mat <- do.call(cbind, lapply(pred_per_rep, function(x) x[[i_fun]]))
+      pred_ensemble[[i_fun]] <- apply(mat, 1, eval(parse(text = i_fun)))
+      
+      if (type == "class") {
+        pred_ensemble[[i_fun]] <- prob_to_binary(
+          pred_ensemble[[i_fun]],
+          thresh = ref_calib_tb %>%
+            dplyr::filter(fun == i_fun) %>%
+            dplyr::pull("optim_value"),
+          class_levels = class_levels
+        )
+      }
+    }
+    
+    pred_ensemble <- data.frame(pred_ensemble)
+  } else {
+    pred_ensemble <- NULL
+  }
+  
+  # add repeat-level/member predictions if requested
+  if (members) {
+    member_pred <- lapply(seq_along(pred_per_rep), function(i_rep) {
+      x <- pred_per_rep[[i_rep]]
+      
+      if (have_fun) {
+        x <- x[, !names(x) %in% fun, drop = FALSE]
+      }
+      
+      if (ncol(x) > 0) {
+        names(x) <- paste0(names(pred_per_rep)[i_rep], ".", names(x))
+      }
+      
+      x
+    })
+    
+    member_pred <- dplyr::bind_cols(member_pred)
+    
+    if (!is.null(pred_ensemble)) {
+      pred_ensemble <- dplyr::bind_cols(pred_ensemble, member_pred)
+    } else {
+      pred_ensemble <- member_pred
+    }
+  }
+  
+  return(pred_ensemble)
 }
+
+
+
+
+
+
 
 
 
@@ -128,5 +385,11 @@ predict.repeat_stack <- function(object,
   names(out) <- fun
   return(as.data.frame(out))
 }
+
+
+# TODO appply all the things from predict_simple_ensemble, such as weighted mean/median
+
+
+
 
 
